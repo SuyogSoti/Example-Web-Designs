0c91faed119091b64005e44a250756cc167918b5
Suyog Soti <suyog.soti@gmail.com>
2017-05-18 21:59:07 -0600

    added disappearing pages

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $("body, div#first").css("height", window.innerHeight);
+    $("div.single").click(function(){
+        $("div#first").fadeOut(function(){
+            $("div#second").fadeIn("slow");
+        });
+    })
 })
