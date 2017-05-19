84afea819e3ff9a224a20caa69884930a7d7e9cc
Suyog Soti <suyog.soti@gmail.com>
2017-05-18 20:57:24 -0600

    cont and background

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
 $(document).ready(function(){
-    $("body, div#first").css("height", window.innerHeight)
-    // $("body, div#first").css("height", window.innerWidth)    
+    $("body, div#first").css("height", window.innerHeight);
 })
