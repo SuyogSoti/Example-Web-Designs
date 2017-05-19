76bf5682c0775d60a5cfa7538caff55ba6e47d45
Suyog Soti <suyog.soti@gmail.com>
2017-05-18 22:11:24 -0600

    added temporary go back

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
     $("body, div#first").css("height", window.innerHeight);
-    $("div.single").click(function(){
+    $("div#first div.single").click(function(){
         $("div#first").fadeOut(function(){
-            $("div#second").fadeIn("slow");
+            $("div#second").fadeIn();
         });
+    })
+    $("div#back").click(function(){
+        $("div#second").fadeOut(function(){
+            $("div#first").fadeIn()
+        })
     })
 })
